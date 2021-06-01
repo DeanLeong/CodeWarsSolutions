c4b0a6a189db7b4f0fe29e4ee1a956bae48fef52
DeanLeong <dean.g.leong@gmail.com>
2021-06-01 15:17:24 -0400

    updated 6/1 solution

--- binaryaddition.js
+++ binaryaddition.js
@@ -1,4 +1,5 @@
-function addBinary(a,b) {
+//My not completely working solution
+function addBinary(a, b) {
   let total = a + b
   
  function total2bin(total) {
@@ -7,5 +8,14 @@ function addBinary(a,b) {
   
   return total2bin(total)
 }
+
+//working solution
+function addBinary(a,b){
+  return (a+b).toString(2)
+}
+
+//my own solution was breaking for some reason.
+//toString(2) takes base 10 numbers and converts them to binary basically
+//numbers 2-10 can be added to toString() to make different conversions
 
 //https://www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript
