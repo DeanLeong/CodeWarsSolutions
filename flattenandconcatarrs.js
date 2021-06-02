bf0306ad6a881345a75328286605ca89bb1fa308
DeanLeong <dean.g.leong@gmail.com>
2021-06-02 14:07:59 -0400

    6/2

---
+++ flattenandconcatarrs.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function flattenAndSort(array) {
+  //flattens the arrays with .concat and .apply
+  //there is a higher order function created to do this which is .flat() but it is not browser supported yet.
+  //from what I understand the .apply part is applying the concat?
+  let flatArr = [].concat.apply([], array)
+  
+  //sort function that correctly sorts numbers applied to flatArr
+  flatArr.sort(function (a, b) {return a - b})
+  
+  //return the flatArr
+  return flatArr;
+}
